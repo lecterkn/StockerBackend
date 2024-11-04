200f41e3f85e7f90dd4006af6ac3bda234a946c4
lecter <lecterkn@proton.me>
2024-11-04 21:24:25 +0900

    gofmt

--- internal/stocker/wire_gen.go
+++ internal/stocker/wire_gen.go
@@ -7,24 +7,24 @@
 package stocker
 
 import (
-    "github.com/google/wire"
-    "h11/backend/internal/stocker/application/service"
-    "h11/backend/internal/stocker/infrastructure/database"
-    "h11/backend/internal/stocker/infrastructure/repository/implements"
-    "h11/backend/internal/stocker/presentation/controller"
+	"github.com/google/wire"
+	"h11/backend/internal/stocker/application/service"
+	"h11/backend/internal/stocker/infrastructure/database"
+	"h11/backend/internal/stocker/infrastructure/repository/implements"
+	"h11/backend/internal/stocker/presentation/controller"
 )
 
 // Injectors from wire.go:
 
 func InitializeController() *ControllersSet {
-    db := database.GetMySQLConnection()
-    itemRepositoryImpl := implements.NewItemRepositoryImpl(db)
-    itemService := service.NewItemService(itemRepositoryImpl)
-    itemController := controller.NewItemController(itemService)
-    controllersSet := &ControllersSet{
-        ItemController: itemController,
-    }
-    return controllersSet
+	db := database.GetMySQLConnection()
+	itemRepositoryImpl := implements.NewItemRepositoryImpl(db)
+	itemService := service.NewItemService(itemRepositoryImpl)
+	itemController := controller.NewItemController(itemService)
+	controllersSet := &ControllersSet{
+		ItemController: itemController,
+	}
+	return controllersSet
 }
 
 // wire.go:
@@ -38,5 +38,5 @@ var serviceSet = wire.NewSet(service.NewItemService)
 var controllerSet = wire.NewSet(controller.NewItemController)
 
 type ControllersSet struct {
-    ItemController controller.ItemController
+	ItemController controller.ItemController
 }
