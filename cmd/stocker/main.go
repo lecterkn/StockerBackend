72be2d614cfd31172af844f08b7b6605a2f1b9d8
lecter <lecterkn@proton.me>
2024-11-04 20:23:22 +0900

    ポート番号を環境変数から設定

--- cmd/stocker/main.go
+++ cmd/stocker/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"h11/backend/internal/stocker"
+	"os"
 )
 
 func main() {
@@ -12,6 +13,12 @@ func main() {
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println(err.Error())
+		return
+	}
+
+	port, ok := os.LookupEnv("ECHO_SERVER_PORT")
+	if !ok {
+		fmt.Println("\"ECHO_SERVER_PORT\" is not set")
 		return
 	}
 
@@ -24,5 +31,5 @@ func main() {
 	e.GET("/items", controllerSets.ItemController.Index)
 	e.POST("/items", controllerSets.ItemController.Create)
 
-	e.Start(":")
+	e.Start(":" + port)
 }
